0aa7acede9f60a6c41c312995cca1a40c5c7c8ec
Enzo Zerega <eczerega@wificlient-10-201-2-63.uc.cl>
2016-03-15 18:37:47 -0300

    asdasd

---
+++ app/controllers/validador_controller.rb
@@ -0,0 +1,42 @@
+require 'base64'
+require 'openssl'
+class ValidadorController < ApplicationController
+	def validar
+	    @mensaje = params[:mensaje].to_s
+	    @hash = params[:hash].to_s
+	    @encoded_string = Digest::SHA256.hexdigest @mensaje
+	    #@encoded_string = @encoded_string.chomp
+	    puts @hash
+	    puts @mensaje
+	    if @hash!='' && @mensaje != ''
+	    	begin 
+			    @retorner = false
+		    	if @hash == @encoded_string
+		    		@retorner =true
+		    	end
+		    	@response = {:mensaje => @mensaje, :valido => @retorner, :status => 200}	
+		    	render json: @response.to_json
+	    	rescue Exception => e
+		    	render json: {
+				  error: "Error del servidor",
+				  status: 500
+				}, status: 500	
+	    	end
+
+    	else
+	    	render json: {
+			  error: "Request no tiene parametros correctos",
+			  status: 400
+			}, status: 400	
+	    end
+
+	end
+
+	def status
+		render json: {
+		  status: 201
+		}, status: 201	
+	end
+
+
+end
